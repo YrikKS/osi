5300deca953b88bfa4e0519ee48fbfbb8f1b6f3d
y.kurgin <y.kurgin@g.nsu.ru>
2022-09-05 22:18:59 +0700

    ready lab3

---
+++ lab3/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <cerrno>
+#include <unistd.h>
+
+typedef struct {
+    std::string str;
+} string_container;
+
+
+void *childFunc(void *arg) {
+    string_container *str = (string_container *) (arg);
+    std::cout << str->str << std::endl;
+    return ((void *) 0);
+}
+
+void startPthread(std::string string) {
+    pthread_t pThread;
+    string_container container;
+    container.str = string;
+    if (pthread_create(&pThread, NULL, childFunc, (void*)&container) {
+        std::cout << "Error: " << std::endl;
+        perror("failed to create process");
+        return 0;
+    } else {
+        int status_addr;
+        pthread_join(pThread, (void **) &status_addr);
+        std::cout << "Child end with code: " << status_addr << std::endl;
+    }
+}
+
+int main() {
+    startPthread("Hello I'm first");
+    startPthread("Hello I'm second");
+    startPthread("Hello I'm third");
+    startPthread("Hello I'm fourth");
+
+    pthread_exit(NULL);
+}
