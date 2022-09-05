ea5d4f9417ab16f7ecb577ac205033576029b4bc
y.kurgin <y.kurgin@g.nsu.ru>
2022-09-05 21:46:23 +0700

    test lab2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <cerrno>
+#include <unistd.h>
+
+void* childFunc(void* arg) {
+    for(int i = 0; i < 10; i++)
+        std::cout << "Hello, World! I'm child " << pthread_self() << " " <<  getpid() << std::endl;
+    return ((void*)0);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    pthread_t pThread;
+    std::cout << "Hello, World! I'm not parent now " << pthread_self() << " " << getpid() << std::endl;
+
+    if(pthread_create(&pThread, NULL, childFunc, NULL)) {
+        std::cout << "I'm not parent ((( " << pthread_self() << " Error: ";
+        perror("faild to create process");
+        return 0;
+    }
+    else {
+        pthread_join(pThread, NULL);
+        std::cout << "I'm have new child " << pThread << std::endl;
+        for(int i = 0; i < 10; i++)
+            std::cout << "Hello, World! I'm parent " << pthread_self() << std::endl;
+    }
+    //sleep(1);
+
+    pthread_exit(NULL);
 }
