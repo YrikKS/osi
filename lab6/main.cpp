8e3de4cf7956cd7777cc3452b4ae02d3f19f0b00
y.kurgin <y.kurgin@g.nsu.ru>
2022-09-28 13:39:16 +0700

    init lab6

---
+++ lab6/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void* childFunc(void* arg) {
+    for(int i = 0; i < 10; i++)
+        std::cout << "Hello, World! I'm child " << pthread_self() << " " <<  getpid() << std::endl;
+    return ((void*)0);
+}
+
+int main() {
+    FILE *file;
+    file = fopen("text.txt", "r");
+
+    char str[20];
+    while (fscanf(file, "%s", str) != EOF) {
+        std::cout << str << std::endl;
+    }
+
+    pthread_t pThread;
+    std::cout << "Hello, World! I'm not parent now " << pthread_self() << " " << getpid() << std::endl;
+
+    if(pthread_create(&pThread, NULL, childFunc, NULL)) {
+        std::cout << "I'm not parent ((( " << pthread_self() << " Error: ";
+        perror("faild to create process");
+        return 0;
+    }
+    else {
+        std::cout << "I'm have new child " << pThread << std::endl;
+        for(int i = 0; i < 10; i++)
+            std::cout << "Hello, World! I'm parent " << pthread_self() << std::endl;
+    }
+    //sleep(1);
+
+    pthread_exit(NULL);
+}
