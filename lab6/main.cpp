a20e8642f69e380705ec60b27fbea9ec07d88bcc
y.kurgin <y.kurgin@g.nsu.ru>
2022-09-28 14:56:52 +0700

    init lab6

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -7,7 +7,7 @@
 
 
 typedef struct {
-    char *str;
+    char str[30];
     int length;
 } my_string;
 
@@ -16,8 +16,7 @@ void *childFunc(void *arg) {
 //    printf("%d\n", myString->length);
     sleep(myString->length);
 //    for(int i = 0; i < myString->length; i++)
-        printf("%s", myString->str);
-    printf("\n");
+    printf("%s\n", myString->str);
 //    std::cout << myString->str << std::endl;
     return ((void *) 0);
 }
@@ -26,18 +25,18 @@ int main() {
     FILE *file;
     file = fopen("text.txt", "r");
 
-    char *str = (char *)malloc(sizeof(char) * 30);
-    while (fgets(str, 30, file) != NULL) {
+    my_string myString[100];
+    int i = 0;
+    while (fgets(myString[i].str, 30, file) != NULL) {
         pthread_t pThread;
-        my_string myString;
-        myString.str = str;
-        myString.length = strlen(str);
-        printf("%s\n", myString.str);
+        myString[i].length = strlen(myString[i].str);
+//        printf("%s\n", myString.str);
         if (pthread_create(&pThread, NULL, childFunc, (void *) &myString)) {
             std::cout << "Error: " << std::endl;
             perror("failed to create pThread");
             return 1;
         }
+        i++;
     }
     sleep(20);
     fclose(file);
