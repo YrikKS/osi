b39f068838bd24b1a0c859e864c1bdfcdb16e6c6
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-03 22:51:23 +0700

    initialize lab17

---
+++ lab17/PthreadSaveList.h
@@ -0,0 +1,38 @@
+//
+// Created by kurya on 03.10.2022.
+//
+
+#ifndef LAB17_PTHREADSAVELIST_H
+#define LAB17_PTHREADSAVELIST_H
+
+#include <cstdlib>
+#include <pthread.h>
+#include <iostream>
+
+template<typename T>
+class PthreadSaveList {
+    typedef struct ElementList {
+        T value;
+        ElementList* next;
+        ElementList* before;
+    } ElementList;
+public:
+    void addBegin(T element);
+
+    ElementList *head;
+
+    PthreadSaveList();
+
+    void printList();
+
+    virtual ~PthreadSaveList();
+
+private:
+    void lockMutex();
+    void unlockMutex();
+    pthread_mutex_t mutex_;
+};
+
+
+
+#endif //LAB17_PTHREADSAVELIST_H
