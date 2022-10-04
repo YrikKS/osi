665be85ae84fb84d9406da4d9649f222766a5c18
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-04 21:38:41 +0700

    initialize lab13

--- lab13/main.cpp
+++ lab13/main.cpp
@@ -108,16 +108,13 @@ int main() {
         pthread_cond_destroy(&condition);
         return 1;
     } else {
-//        int childFuncErrorCode = 0;
-        void* childFuncErrorCode = childFunc((void *) &settingsForMainThread);
-        std::cout << "childFuncErrorCode" << std::endl;
-        std::cout << ((int*)childFuncErrorCode) << std::endl;
-//        if (childFuncErrorCode != SUCCESS) {
-//            fprintf(stderr, "Error in printer function: ", childFuncErrorCode);
-//            pthread_mutex_destroy(&mutexOne);
-//            pthread_cond_destroy(&condition);
-//            return 1;
-//        }
+        int* childFuncErrorCode = (int*)childFunc((void *) &settingsForMainThread);
+        if (childFuncErrorCode != SUCCESS) {
+            fprintf(stderr, "Error in printer function: ", childFuncErrorCode);
+            pthread_mutex_destroy(&mutexOne);
+            pthread_cond_destroy(&condition);
+            return 1;
+        }
     }
 
     int errorCode = 0;
