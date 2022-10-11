ae6aaf238bb5be1d21b80638a76a33347c6a8b72
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-11 15:58:42 +0700

    lab23 init

---
+++ lab23/PthreadSaveList.h
@@ -0,0 +1,194 @@
+//
+// Created by kurya on 03.10.2022.
+//
+
+#ifndef LAB17_PTHREADSAVELIST_H
+#define LAB17_PTHREADSAVELIST_H
+
+#define SUCCESS 0
+
+#include <cstdlib>
+#include <pthread.h>
+#include <iostream>
+#include <stdio.h>
+#include <cerrno>
+
+template<typename T>
+class PthreadSaveList {
+    typedef struct ElementList {
+        T value;
+        ElementList *next;
+        ElementList *prev;
+    } ElementList;
+public:
+    ElementList *head;
+
+    void addBegin(T element);
+
+    PthreadSaveList();
+
+    void printList();
+
+    void sortList();
+
+    void addEnd(T element);
+
+    virtual ~PthreadSaveList();
+
+private:
+    void lockMutex();
+
+    int getListLengthNotThreadSave();
+
+    void swapElementNotThreadSave(ElementList *element1, ElementList *element2);
+
+    void unlockMutex();
+
+    pthread_mutex_t mutex_;
+};
+
+template<typename T>
+void PthreadSaveList<T>::printList() {
+    lockMutex();
+    int iteration = 0;
+    for (ElementList *elementList = head; elementList != NULL; elementList = elementList->next) {
+        if (iteration == 0) {
+            std::cout << elementList->value;
+        } else {
+            std::cout << " - " << elementList->value;
+        }
+        iteration++;
+    }
+    std::cout << std::endl;
+    unlockMutex();
+}
+
+template<typename T>
+void PthreadSaveList<T>::addBegin(T element) {
+    lockMutex();
+    ElementList *newElement = new ElementList;
+    ElementList *secondElement = head;
+    newElement->value = element;
+    newElement->prev = NULL;
+    newElement->next = secondElement;
+    head = newElement;
+    if (secondElement != NULL) {
+        secondElement->prev = newElement;
+    }
+    unlockMutex();
+}
+
+template<typename T>
+PthreadSaveList<T>::PthreadSaveList() : head(NULL) {
+    int mutexInitErrorCode = pthread_mutex_init(&mutex_, NULL);
+    if (mutexInitErrorCode != SUCCESS) {
+        fprintf(stderr, "Error mutex lock: ", mutexInitErrorCode);
+        exit(1);
+    }
+}
+
+template<typename T>
+void PthreadSaveList<T>::lockMutex() {
+    int mutexLockErrorCode = pthread_mutex_lock(&mutex_);
+    if (mutexLockErrorCode != SUCCESS) {
+        fprintf(stderr, "Error mutex lock: ", mutexLockErrorCode);
+        exit(1);
+    }
+}
+
+template<typename T>
+void PthreadSaveList<T>::unlockMutex() {
+    int mutexUnLockErrorCode = pthread_mutex_unlock(&mutex_);
+    if (mutexUnLockErrorCode != SUCCESS) {
+        fprintf(stderr, "Can't unlock mutex: ", mutexUnLockErrorCode);
+        exit(1);
+    }
+}
+
+template<typename T>
+PthreadSaveList<T>::~PthreadSaveList() {
+//    ElementList *elementCopy;
+    for (ElementList *element = head; element != NULL;) {
+        if (element->next != NULL) {
+            element = element->next;
+            delete element->prev;
+        } else {
+            delete element;
+            break;
+        }
+    }
+    int mutexDestroyErrorCode = pthread_mutex_destroy(&mutex_);
+    if (mutexDestroyErrorCode != SUCCESS) {
+        fprintf(stderr, "Error mutex destroy: ", mutexDestroyErrorCode);
+    }
+}
+
+
+template<typename T>
+void PthreadSaveList<T>::sortList() {
+    lockMutex();
+    int countElement = getListLengthNotThreadSave();
+    for (int i = 0; i < countElement; i++) {
+        ElementList *elementList = head;
+
+        for (int j = 0; j < countElement - i - 1; j++) {
+            if (elementList->value > elementList->next->value) {
+                swapElementNotThreadSave(elementList, elementList->next);
+            } else {
+                elementList = elementList->next;
+            }
+        }
+    }
+    unlockMutex();
+}
+
+template<typename T>
+void PthreadSaveList<T>::swapElementNotThreadSave(ElementList *element1, ElementList *element2) {
+    if (element1 == head) {
+        head = element2;
+    }
+    element1->next = element2->next;
+    element2->prev = element1->prev;
+    element1->prev = element2;
+    element2->next = element1;
+    if (element1->next != NULL) {
+        element1->next->prev = element1;
+    }
+    if (element2->prev != NULL) {
+        element2->prev->next = element2;
+    }
+}
+
+template<typename T>
+int PthreadSaveList<T>::getListLengthNotThreadSave() {
+    ElementList *elementList = head;
+    int countElement = 0;
+    while (elementList != NULL) {
+        countElement++;
+        elementList = elementList->next;
+    }
+    return countElement;
+}
+
+template<typename T>
+void PthreadSaveList<T>::addEnd(T element) {
+    lockMutex();
+    ElementList *newElement = new ElementList;
+    ElementList *secondElement = head;
+    newElement->value = element;
+    newElement->next = NULL;
+    if (secondElement != NULL) {
+        for (; secondElement->next != NULL;) {
+            secondElement = secondElement->next;
+        }
+        secondElement->next = newElement;
+        newElement->prev = secondElement;
+    } else {
+        head = newElement;
+        newElement->prev = NULL;
+    }
+    unlockMutex();
+}
+
+
+#endif //LAB17_PTHREADSAVELIST_H
