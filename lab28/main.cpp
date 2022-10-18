5e101dd02df612280aa9d4afd5a9f1508f5816c0
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-18 16:03:09 +0700

    init lab28

---
+++ lab28/main.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/termios.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    char pszRequest[100] = {0};
+    char pszResourcePath[] = "";
+    char pszHostAddress[] = "https://www.opennet.ru/man.shtml?topic=socket&category=2&russian=0";
+    sprintf(pszRequest, "GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/plain\r\n\r\n", pszResourcePath,
+            pszHostAddress);
+    printf("Created Get Request is below:\n\n\n");
+
+    char ADDRESS[] = "https://www.opennet.ru/man.shtml?topic=socket&category=2&russian=0";
+    int sockfd = 0;
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0){                                                                                                                                // возращает дискриптор sockfd ()
+        perror("client socket");
+        return 1;
+    }
+
+    struct sockaddr_un sockAddress;                                                                                                                                              // Адрес доменного сокета UNIX
+    memset(&sockAddress, 0, sizeof(struct sockaddr_un));                                                                                                                                    //инициализируем переменную нулями
+
+    sockAddress.sun_family = PF_INET;                                                                                                                                                // заполняем поле sun_family
+    int addressLen = strlen(ADDRESS);                                                                                                                                               // узнаем размер ADDRESS
+    strncpy(sockAddress.sun_path, ADDRESS, addressLen);
+
+    if (connect(sockfd, (struct sockaddr*) &sockAddress, sizeof(sockAddress)) < 0){
+        perror("connect");
+        return 2;
+    }
+
+    std::cout << "all good" << std::endl;
+    close(sockfd);
+    char buffer[BUFFER_SIZE] = {0};
+
+    return 0;
+}
