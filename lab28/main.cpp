7ca4650d710f18ce4b82cb3bd2aa842148857c7b
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-18 20:43:35 +0700

    init lab28

--- lab28/main.cpp
+++ lab28/main.cpp
@@ -52,7 +52,8 @@ int main(int argc, char *argv[]){
 
     fd = socket_connect(argv[1], atoi(argv[2]));
     char pszRequest[100]= {0};
-    char pszResourcePath[] = "";
+    char pszResourcePath[] = "Dross0/OS-Labs/blob/master/HTTP%20Client%201/http.c";
+//    https://github.com/Dross0/OS-Labs/blob/master/HTTP%20Client%201/http.c
     char* pszHostAddress = argv[1];
     sprintf(pszRequest, "GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/plain\r\n\r\n", pszResourcePath, pszHostAddress);
     std::cout << pszRequest << std::endl;
