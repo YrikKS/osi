4086b1999318dbc7946e4dfdf5d61b279fb04edf
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-18 16:56:02 +0700

    init lab28

--- lab28/main.cpp
+++ lab28/main.cpp
@@ -50,11 +50,11 @@ int main(int argc, char *argv[]){
     }
 
     fd = socket_connect(argv[1], atoi(argv[2]));
-    char request[100] = {0};
-    snprintf(request, 100, "GET %s HTTP/1.1\nHost: %s\n\n",
+    char request[1000] = {0};
+    snprintf(request, 1000, "GET %s HTTP/1.1\nHost: %s\n\n",
              "WackoWiki/KursOperacionnyeSistemy/PraktikumPosixThreads/PthreadTasks", "parallels.nsu.ru");
 //    http://parallels.nsu.ru/WackoWiki/KursOperacionnyeSistemy/PraktikumPosixThreads/PthreadTasks
-    send(fd, request, strlen(request), 0); // write(fd, char[]*, len);
+    write(fd, request, strlen(request)); // write(fd, char[]*, len);
     bzero(buffer, BUFFER_SIZE);
 
     while(read(fd, buffer, BUFFER_SIZE - 1) != 0){
