7bcb4f6700e028c07bf6a177ce4069dfdffb7664
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-18 23:36:27 +0700

    start lab28

--- lab28/main.cpp
+++ lab28/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char *argv[]) {
 //    sprintf(pszRequest, "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", pszResourcePath, pszHostAddress);
 //    std::cout << pszRequest << std::endl;
 
-    write(fd, "GET /\r\n", strlen("GET /\r\n")); // write(fd, char[]*, len);
+    write(fd, "GET / HTTP/1.1\r\n", strlen("GET / HTTP/1.1\r\n")); // write(fd, char[]*, len);
     bzero(buffer, BUFFER_SIZE);
 
     while(read(fd, buffer, BUFFER_SIZE - 1) != 0){
