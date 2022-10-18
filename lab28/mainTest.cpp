0bd571788c42096cf7f764f64de74382bfb548e3
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-18 23:00:04 +0700

    start lab28

--- lab28/mainTest.cpp
+++ lab28/mainTest.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]){
     char pszResourcePath[] = "";
 //    https://parallel.uran.ru/book/export/html/474
     char* pszHostAddress = argv[1];
-    sprintf(pszRequest, "GET /%s HTTPS/1.1\r\nHost: %s\r\n\r\n", pszResourcePath, pszHostAddress);
+    sprintf(pszRequest, "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", pszResourcePath, pszHostAddress);
     std::cout << pszRequest << std::endl;
 
     write(fd, pszRequest, strlen(pszRequest)); // write(fd, char[]*, len);
