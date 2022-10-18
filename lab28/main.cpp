055d3e06c5fbe71bb9af9ec773177a062f41bd42
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-18 23:25:25 +0700

    start lab28

--- lab28/main.cpp
+++ lab28/main.cpp
@@ -4,21 +4,21 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-//#include <netinet/tcp.h>
-//#include <sys/socket.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
 #include <sys/types.h>
-//#include <netinet/in.h>
-//#include <netdb.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <iostream>
+#define BUFFER_SIZE 1024
 
-
-std::string parseUrl(char* url) {
+std::string parseUrl(char *url) {
     int sizeUrl = strlen(url);
     std::string strUrl = url;
     std::string subSting;
-    if(strUrl.find("http://") != strUrl.npos) {
+    if (strUrl.find("http://") != strUrl.npos) {
         subSting = strUrl.substr(7);
-    } else if(strUrl.find("https://") != strUrl.npos) {
+    } else if (strUrl.find("https://") != strUrl.npos) {
         subSting = strUrl.substr(8);
     } else {
         subSting = strUrl;
@@ -26,12 +26,58 @@ std::string parseUrl(char* url) {
     return subSting;
 }
 
+int connectSocket(std::string url) {
+    struct hostent *hostent = gethostbyname(url.data());
+    if (hostent == NULL) {
+        herror("gethostbyname");
+        exit(1);
+    }
+
+    struct sockaddr_in sockAddr;
+    bcopy(hostent->h_addr, &sockAddr.sin_addr, hostent->h_length);
+    sockAddr->sin_port = 80;
+    sockAddr.sin_family = AF_INET;
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0); //PF_INET
+    if (sock == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    if (connect(sock, (struct sockaddr *) &sockAddr, sizeof(struct sockaddr_in)) == -1) {
+        perror("connect");
+        exit(1);
+    }
+    return sock;
+}
+
 int main(int argc, char *argv[]) {
-    if(argc != 2) {
+    if (argc != 2) {
         std::cout << "Incorrect args!\n exampl: " << argv[0] << "<https://<hostName>" << std::endl;
         return 1;
     }
-
     std::string url = parseUrl(argv[1]);
-    std::cout << url << std::endl;
+    int sock = connectSocket(url);
+    int fd;
+    char buffer[BUFFER_SIZE];
+    int fd = connectSocket(url);
+    char pszRequest[100]= {0};
+    char pszResourcePath[] = "";
+//    https://parallel.uran.ru/book/export/html/474
+    char* pszHostAddress = argv[1];
+    sprintf(pszRequest, "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", pszResourcePath, pszHostAddress);
+    std::cout << pszRequest << std::endl;
+
+    write(fd, pszRequest, strlen(pszRequest)); // write(fd, char[]*, len);
+    bzero(buffer, BUFFER_SIZE);
+
+    while(read(fd, buffer, BUFFER_SIZE - 1) != 0){
+        fprintf(stderr, "%s", buffer);
+        bzero(buffer, BUFFER_SIZE);
+    }
+
+    shutdown(fd, SHUT_RDWR);
+    close(fd);
+
+    return 0;
 }
