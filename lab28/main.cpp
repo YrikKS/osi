dbf543f181411a66dd0a35d342f2dfdb93eb4bde
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-18 16:31:26 +0700

    init lab28

--- lab28/main.cpp
+++ lab28/main.cpp
@@ -15,30 +15,33 @@ int main(int argc, char *argv[]) {
     sprintf(pszRequest, "GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/plain\r\n\r\n", pszResourcePath,
             pszHostAddress);
     printf("Created Get Request is below:\n\n\n");
-
-    char ADDRESS[] = "https://www.opennet.ru/man.shtml?topic=socket&category=2&russian=0";
-    int sockfd = 0;
-    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0){                                                                                                                                // возращает дискриптор sockfd ()
-        perror("client socket");
-        return 1;
-    }
-
-    struct sockaddr_un sockAddress;                                                                                                                                              // Адрес доменного сокета UNIX
-    memset(&sockAddress, 0, sizeof(struct sockaddr_un));                                                                                                                                    //инициализируем переменную нулями
-
-    sockAddress.sun_family = PF_INET;                                                                                                                                                // заполняем поле sun_family
-    int addressLen = strlen(ADDRESS);                                                                                                                                               // узнаем размер ADDRESS
-    strncpy(sockAddress.sun_path, ADDRESS, addressLen);
-
-
-    std::cout << "connect" << std::endl;
-    if (connect(sockfd, (struct sockaddr*) &sockAddress, sizeof(sockAddress)) < 0){
-        perror("connect");
-        return 2;
-    }
-
-    std::cout << "all good" << std::endl;
-    close(sockfd);
+    struct hostent *hp;
+    hp = gethostbyname(pszHostAddress);
+    std::cout << hp->h_addr << std::endl;
+//
+//    char ADDRESS[] = "https://www.opennet.ru/man.shtml?topic=socket&category=2&russian=0";
+//    int sockfd = 0;
+//    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0){                                                                                                                                // возращает дискриптор sockfd ()
+//        perror("client socket");
+//        return 1;
+//    }
+//
+//    struct sockaddr_un sockAddress;                                                                                                                                              // Адрес доменного сокета UNIX
+//    memset(&sockAddress, 0, sizeof(struct sockaddr_un));                                                                                                                                    //инициализируем переменную нулями
+//
+//    sockAddress.sun_family = PF_INET;                                                                                                                                                // заполняем поле sun_family
+//    int addressLen = strlen(ADDRESS);                                                                                                                                               // узнаем размер ADDRESS
+//    strncpy(sockAddress.sun_path, ADDRESS, addressLen);
+//
+//
+//    std::cout << "connect" << std::endl;
+//    if (connect(sockfd, (struct sockaddr*) &sockAddress, sizeof(sockAddress)) < 0){
+//        perror("connect");
+//        return 2;
+//    }
+//
+//    std::cout << "all good" << std::endl;
+//    close(sockfd);
 //    send(sockfd, pszRequest, 100, 0);
 //    char buffer[1024] = {0};
 
