e932a4fddc6c1b0bcd1bb45df7ce10e04d81d612
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-25 23:46:30 +0700

    test lab28

--- lab28/main2.cpp
+++ lab28/main2.cpp
@@ -148,7 +148,7 @@ int main(int argc, char *argv[]) {
                     if (c == '\n' && currentWriteBuf < currentReadBuf) {
                         fprintf(stdout, "%s", bufferFromRead[currentWriteBuf]);
                         currentWriteBuf++;
-                        std::cout << currentWriteBuf << std::endl;
+                        std::cout << "currentWriteBuf" << currentWriteBuf << std::endl;
                         std::cout << std::endl << "Press enter to scroll down" << std::endl;
                     } else if (c == '\n') {
                         std::cout << "pleas wait data" << std::endl;
