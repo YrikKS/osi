0ceab5a84ff6e4317bf897aa62ef2beda86c5425
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-26 13:19:18 +0700

    init lab4

---
+++ lab4/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <cerrno>
+#include <unistd.h>
+
+void *childFunc(void *arg) {
+    while (true) {
+        pthread_testcancel();
+        std::cout << "live" << std::endl;
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t pThread;
+
+    if (pthread_create(&pThread, NULL, childFunc, NULL)) {
+        perror("Fail to create process");
+        return 0;
+    }
+
+    sleep(2);
+    int cancelError = pthread_cancel(pThread);
+    if (cancelError != 0) {
+        std::cout << "Cancel error: ESRCH" << std::endl;
+    }
+
+    return 0;
+}
