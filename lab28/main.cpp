1940de68c6f415b1ed4e3f08ed0da593d5991148
y.kurgin <y.kurgin@g.nsu.ru>
2022-10-31 22:38:43 +0700

    test lab28

--- lab28/main.cpp
+++ lab28/main.cpp
@@ -94,7 +94,7 @@ std::string getDomain(std::string url) {
 void addToBuffer(std::vector<std::string> *vectorReadStrings, char *readBuf, std::string *restOfTheLine) {
     std::string readStrings(readBuf);
     int indexEnter = readStrings.find('\n', 0);
-    vectorReadStrings->push_back((*restOfTheLine) + readStrings.substr(0, indexEnter));
+    vectorReadStrings->push_back((*restOfTheLine) + readStrings.substr(0, indexEnter + 1));
 //    std::cout << (*vectorReadStrings)[vectorReadStrings->size()-1] << std::endl;
     int lastIndex = readStrings.find('\n', indexEnter + 1);
 
