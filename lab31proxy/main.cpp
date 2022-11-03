cf1b0c02ff6d1bf45394dad9f6ff6d1906d48fff
y.kurgin <kurgin.ys@gmail.com>
2022-11-04 00:19:54 +0700

    check boost

--- lab31proxy/main.cpp
+++ lab31proxy/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "ServerImpl.h"
-
+#include <boost/version.hpp>
 
 int main() {
+    std::cout << "Using Boost "
+              << BOOST_VERSION / 100000     << "."  // major version
+              << BOOST_VERSION / 100 % 1000 << "."  // minor version
+              << BOOST_VERSION % 100                // patch level
+              << std::endl;
     ProxyServer::ServerImpl server = ProxyServer::ServerImpl();
     server.startServer();
     return 0;
