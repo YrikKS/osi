6dfbd9da88d2e8a4aeb7c4acfb80d7c41774b771
y.kurgin <kurgin.ys@gmail.com>
2022-11-04 15:25:02 +0700

    main update

--- lab31proxy/ServerImpl.cpp
+++ lab31proxy/ServerImpl.cpp
@@ -33,19 +33,16 @@ void ServerImpl::startServer() {
 
 
 void ServerImpl::updatePollFd() {
-//    LOG_EVENT("update pollSet");
+    LOG_EVENT("update pollSet");
     memset(_pollSet, 0, MAX_COUNT_CONNECTIONS * sizeof(struct pollfd));
     _pollSet[0].fd = _serverSocket->getFdSocket();
     _pollSet[0].events = POLLIN;
 
     int i = 1;
-    std::cout << "helo1" << std::endl;
     for (auto it = _clientList.begin(); it != _clientList.end(); it++, i++) {
-        std::cout << "helo2" << std::endl;
         _pollSet[i].fd = (*it)->getFdClient();
         _pollSet[i].events = POLLIN;
     }
-    std::cout << "end updatePollFd" << std::endl;
 }
 
 ServerImpl::ServerImpl() {
@@ -69,13 +66,13 @@ void ServerImpl::handlingEvent() {
 //                isNeedUpdatePollSet = true;
                 updatePollFd();
             } else {
-
+                handlingReadBuf(buf);
             }
         }
     }
-    LOG_EVENT("end handlingEvent");
 }
 
 void ServerImpl::handlingReadBuf(char *buf) {
     std::cout << "handlingEvent" << std::endl;
+    std::cout << buf << std::endl;
 }
