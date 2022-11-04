dd3998dcc16fec4a3ed53d833f99e7c90984bdc0
y.kurgin <kurgin.ys@gmail.com>
2022-11-04 15:10:55 +0700

    main update

--- lab31proxy/main.cpp
+++ lab31proxy/main.cpp
@@ -3,6 +3,7 @@
 #include "logger/Logger.h"
 
 int main() {
+    LOG_EVENT("start program");
     ProxyServer::ServerImpl server = ProxyServer::ServerImpl();
     server.startServer();
     LOGGER_CLOSE;
