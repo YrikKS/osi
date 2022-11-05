92e8c7a85ab1924ba3542380232e6f2cd0e82cc7
y.kurgin <kurgin.ys@gmail.com>
2022-11-05 17:12:30 +0700

    add httpServerSender

--- lab31proxy/server/ServerImpl.cpp
+++ lab31proxy/server/ServerImpl.cpp
@@ -108,7 +108,7 @@ void ServerImpl::handlingReadBuf(char *buf, Client *client) {
 
 //            std::cout << "try send ::: " << client->getClientData()->getRequestHeading() << std::endl;
             char newBuf[1024] = {0};
-            httpServer->sendBuf((client->getClientData()->getRequestHeading()).c_str());
+            httpServer->sendBuf((client->getClientData()->getRequestHeading()).data());
             std::cout << httpServer->readBuf(newBuf) << std::endl;
 
         } else {
