982ee447770e2ec3d431ea2f2159b073ed2f56ea
y.kurgin <kurgin.ys@gmail.com>
2022-11-09 00:47:43 +0700

    deleteCashElement (save)

--- lab31proxy/cash/CashImpl.h
+++ lab31proxy/cash/CashImpl.h
@@ -18,6 +18,8 @@ namespace ProxyServer {
 
         CashElement *addStringToCash(std::string request) override;
 
+        void deleteCashElement(CashElement *cashElement) override;
+
     private:
         std::list<CashElement*> _listCash;
     };
