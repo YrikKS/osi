f6e07cbe554f84fea11a82f8e23bcb2a17289c47
y.kurgin <kurgin.ys@gmail.com>
2022-11-09 20:38:28 +0700

    add httpSender

--- httpReauestSender/writer.cpp
+++ httpReauestSender/writer.cpp
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
     }
 
     char buffer[10000] = {0};
-    while (recv(sock, buffer, BUFFER_SIZE - 1, 0) != 0) {
+    while (recvmsg(sock, buffer, BUFFER_SIZE - 1, 0) != 0) {
         for(int i = 0; i < strlen(buffer); i++) {
             std::cout << buffer[i] << " = " << (int)buffer[i] << "   ";
         }
