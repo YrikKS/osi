c4226a6642d9d63f54cdb047a60eb23bf8aa0af8
y.kurgin <kurgin.ys@gmail.com>
2022-12-12 18:35:16 +0700

    test static connect

--- lab32proxy/socket/ServerSocketImpl.cpp
+++ lab32proxy/socket/ServerSocketImpl.cpp
@@ -88,7 +88,7 @@ ServerSocketImpl::~ServerSocketImpl() {
 }
 
 Client *ServerSocketImpl::connectToClient(std::string url, int port) {
-    pthread_mutex_lock(&ProxyServer::ServerSocketImpl::MutexForServer::mutexForServer);
+    pthread_mutex_lock(&ProxyServer::ServerSocketImpl::mutexForServer);
     struct hostent *hostent = gethostbyname(url.data());
     if (hostent == NULL) {
         LOG_ERROR("gethostbyname");
@@ -115,7 +115,7 @@ Client *ServerSocketImpl::connectToClient(std::string url, int port) {
 
     LOG_EVENT("http server connect");
     Client *client = new ClientImpl(sock, TypeClient::HTTP_SERVER, NULL);
-    pthread_mutex_unlock(&(ProxyServer::ServerSocketImpl::MutexForServer::mutexForServer));
+    pthread_mutex_unlock(&(ProxyServer::ServerSocketImpl::mutexForServer));
     return client;
 }
 
