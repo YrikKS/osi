394cecf31bf695469f4d2ad70be911bb728ce5a6
y.kurgin <kurgin.ys@gmail.com>
2022-12-21 00:22:33 +0700

    add shared_ptr and unique_ptr

--- lab32proxy/main.cpp
+++ lab32proxy/main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include "server/NewServerImpl.h"
+#include <memory>
 #include <signal.h>
 
 int main(int argc, char *argv[]) {
     LOG_EVENT("start program");
     signal(SIGPIPE, SIG_IGN);
-    ProxyServer::Server *server = nullptr;
     try {
-        server = new ProxyServer::NewServerImpl();
+        auto server = std::unique_ptr<ProxyServer::Server>(new ProxyServer::NewServerImpl());
         server->startServer();
     } catch (std::exception &ex) {
         std::cout << ex.what() << std::endl;
         LOG_ERROR("close app by exception");
     }
-    delete server;
     LOG_EVENT("close app");
     return 0;
 }
